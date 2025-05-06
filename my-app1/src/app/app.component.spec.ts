9753463ede9f8693e5e1d2d4d0a60b3f35e0cdf4
praveenkmca <praveenk2882@gmail.com>
2025-05-06 23:42:15 +0530

    initial commit

--- my-app1/src/app/app.component.spec.ts
+++ my-app1/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sampleapp'`, () => {
+  it(`should have as title 'my-app1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sampleapp');
+    expect(app.title).toEqual('my-app1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, sampleapp');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-app1');
   });
 });
